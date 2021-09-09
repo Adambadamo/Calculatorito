d8a1464e66eb9c420eb64aa273df0be051862df6
Adambadamo <urbadam95@gmail.com>
2021-09-09 21:26:25 +0200

    UI + evaluator
    UI update
    + javaluator lib import

--- app/src/main/java/com/example/calculatorito/MainActivity.kt
+++ app/src/main/java/com/example/calculatorito/MainActivity.kt
@@ -2,8 +2,9 @@ package com.example.calculatorito
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fathzer.soft.javaluator.DoubleEvaluator
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.math.exp
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,23 +17,28 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
-        oneButton.setOnClickListener{ calculate("1") }
-        twoButton.setOnClickListener{ calculate("2") }
-        threeButton.setOnClickListener{ calculate("3") }
-        fourButton.setOnClickListener{ calculate("4") }
-        fiveButton.setOnClickListener{ calculate("5") }
-        sixButton.setOnClickListener{ calculate("6") }
-        sevenButton.setOnClickListener{ calculate("7") }
-        eightButton.setOnClickListener{ calculate("8") }
-        nineButton.setOnClickListener{ calculate("9") }
-        dotButton.setOnClickListener{ calculate(".") }
+        zeroButton.setOnClickListener { expressionBuilder("0") }
+        oneButton.setOnClickListener{ expressionBuilder("1") }
+        twoButton.setOnClickListener{ expressionBuilder("2") }
+        threeButton.setOnClickListener{ expressionBuilder("3") }
+        fourButton.setOnClickListener{ expressionBuilder("4") }
+        fiveButton.setOnClickListener{ expressionBuilder("5") }
+        sixButton.setOnClickListener{ expressionBuilder("6") }
+        sevenButton.setOnClickListener{ expressionBuilder("7") }
+        eightButton.setOnClickListener{ expressionBuilder("8") }
+        nineButton.setOnClickListener{ expressionBuilder("9") }
+        dotButton.setOnClickListener{ expressionBuilder(".") }
 
-        addButton.setOnClickListener{calculate("+")}
-        mulButton.setOnClickListener{calculate("-")}
-        mulButton.setOnClickListener{calculate("*")}
-        addButton.setOnClickListener{calculate("/")}
-        addButton.setOnClickListener{calculate("%")}
-        addButton.setOnClickListener{calculate("=")}
+        addButton.setOnClickListener{expressionBuilder("+")}
+        subButton.setOnClickListener{expressionBuilder("-")}
+        mulButton.setOnClickListener{expressionBuilder("*")}
+        divButton.setOnClickListener{expressionBuilder("/")}
+        modButton.setOnClickListener{expressionBuilder("%")}
+
+        eqButton.setOnClickListener{
+            if(expression.text.isNotEmpty()){
+            calculate(expression.text.toString())}
+        }
 
         delButton.setOnClickListener {
 
@@ -40,7 +46,6 @@ class MainActivity : AppCompatActivity() {
             if(string.isNotEmpty()){
                 expression.text = expression.text.substring(0,string.length-1);
             }
-            result.text = "";
 
         }
 
@@ -51,14 +56,16 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun calculate( string:String): String {
+    fun expressionBuilder( string:String) {
 
         expression.append(string)
 
-
-
-        return ""
     }
 
+    fun calculate(string: String){
+
+        var evaluator = DoubleEvaluator()
+        result.text = evaluator.evaluate(expression.text.toString()).toString()
+    }
 
 }
